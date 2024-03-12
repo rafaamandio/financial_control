83d30d82295ea9fc9fed9fbcfa60c47377d3c93d
Rafael Amandio <68651145+rafaamandio@users.noreply.github.com>
2024-03-11 22:07:21 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 app.use("/", express.static("./client"));
 
-const connection = pgp()("postgres://postgres:root@localhost:5432/app");
+const connection = pgp()("postgres://default:************@ep-gentle-cherry-a4qvhr62.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require");
 
 app.get("/api/lancamentos", async function  (req, res) {
 	const lancamentos = await connection.query("select * from financas_pessoais.lancamento", []);
